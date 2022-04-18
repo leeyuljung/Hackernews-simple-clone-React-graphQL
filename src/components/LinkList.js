ca86d60f222a7c2c3f8c4465f1675fdd51e0631b
leeyuljung <rita810605@gmail.com>
2022-04-18 22:29:31 +0800

    Modify: 將 Mock data 改成由資料庫取得並 render 出來

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,27 +1,34 @@
 import React from 'react'
 import Link from './Link'
+import { useQuery, gql } from '@apollo/client'
+
+const FEED_QUERY = gql`
+    {
+        feed {
+            id
+            links {
+                id
+                createdAt
+                url
+                description
+            }
+        }
+    }
+`
 
 const LinkList = () => {
-    // Mock Data
-    const linksToRender = [
-        {
-            id: 'link-id-1',
-            description: 'Prisma gives you a powerful database toolkit',
-            url: 'https://prisma.io'
-        },
-        {
-            id: 'link-id-2',
-            description: 'The best GraphQL client',
-            url: 'https://www.apollographql.com/docs/react/'
-        }
-    ]
+    const { data } = useQuery(FEED_QUERY)
 
     return (
         <div>
             {
-                linksToRender.map(link => (
-                    <Link key={ link.id } link={ link }/>
-                ))
+                data && (
+                    <>
+                        {data.feed.links.map(link => (
+                            <Link key={ link.id } link={ link } />
+                        ))}
+                    </>
+                )
             }
         </div>
     )
