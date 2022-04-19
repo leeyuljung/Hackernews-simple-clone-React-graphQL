b20758fd826c7aab0b112a6e878294ec5d2cb00f
leeyuljung <rita810605@gmail.com>
2022-04-20 01:35:59 +0800

    modify: 根據權限加入登入登出按鈕控制，以及登入後才顯示 submit 按鈕

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { AUTH_TOKEN } from '../constants'
 
 const Header = () => {
+    const navigate = useNavigate()
+    const authToken = localStorage.getItem(AUTH_TOKEN)
+
     return (
         <div className='flex pa1 justify-between nowrap orange'>
             <div className='flex flex-fixed black'>
@@ -9,8 +13,25 @@ const Header = () => {
                     <div className='fw7 mr1'>Hacker News</div>
                 </Link>
                 <Link to="/" className='ml1 no-underline black'>new</Link>
-                <div className='ml1'>|</div>
-                <Link to="/create" className='ml1 no-underline black'>submit</Link>
+                {
+                    authToken && (
+                        <div className='flex'>
+                            <div className='ml1'>|</div>
+                            <Link to="/create" className='ml1 no-underline black'>submit</Link>
+                        </div>
+                    )
+                }
+            </div>
+            <div className='flex flex-fixed'>
+                {
+                    authToken ? 
+                    (
+                        <div className='ml1 pointer black' onClick={() => {localStorage.removeItem(AUTH_TOKEN); navigate('/')}}>logout</div>
+                    ) :
+                    (
+                        <Link to="/login" className='ml1 no-underline black'>login</Link>
+                    )
+                }
             </div>
         </div>
     )
