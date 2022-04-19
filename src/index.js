ead52a6123bb7501b5fc3a9cc37cb8ff00a18c83
leeyuljung <rita810605@gmail.com>
2022-04-20 02:21:10 +0800

    modify: 配置 apollo 以確保能讀懂 client 端傳來的 token

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './styles/index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context'
+import { AUTH_TOKEN } from './constants';
 
 // 將 Apollo client 實例和 GraphQL API 做連接
 const httpLink = createHttpLink({
@@ -11,9 +13,20 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+// middleware, 如果有 token 的話，會回傳 headers，並提供到 context 中
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return{
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 // 實例化 Apollo client
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
