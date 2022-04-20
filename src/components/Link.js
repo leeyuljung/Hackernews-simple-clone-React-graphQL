29ae82cb926ed4267f9edced770eb6fd6a34faf5
leeyuljung <rita810605@gmail.com>
2022-04-20 18:22:35 +0800

    modify: 透過 useMutation 來完成 vote 功能

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,37 @@
 import React from 'react'
 import { AUTH_TOKEN } from '../constants'
 import { timeDifferenceForDate } from '../utils'
+import { useMutation, gql } from '@apollo/client'
+
+const VOTE_MUTATION = gql`
+    mutation VoteMutation($linkId: ID!) {
+        vote(linkId: $linkId) {
+            id
+            link {
+                id
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
+            }
+            user {
+                id
+            }
+        }
+    }
+`
 
 const Link = (props) => {
     const { link } = props
     const authToken = localStorage.getItem(AUTH_TOKEN)
+
+    const [vote] = useMutation(VOTE_MUTATION, {
+        variables: {
+            linkId: link.id
+        }
+    })
 
     return (
         <div className='flex mt2 items-start'>
@@ -12,7 +39,7 @@ const Link = (props) => {
                 <span className='gray'>{ props.index + 1 }</span>
                 {
                     authToken && (
-                        <div className='ml1 gray f11' style={{ cursor: 'pointer' }} onClick={() => console.log('Clicked vote button')}>▲</div>
+                        <div className='ml1 gray f11' style={{ cursor: 'pointer' }} onClick={vote}>▲</div>
                     )
                 }
             </div>
@@ -20,13 +47,15 @@ const Link = (props) => {
                 <div>
                     { link.description } ({ link.url })
                 </div>
-                {(
-                    <div className='f6 lh-copy gray'>
-                        { link.votes.length } votes | by{' '}
-                        { link.postedBy ? link.postedBy.name : 'Unknown' }{' '}
-                        { timeDifferenceForDate(link.createdAt) }
-                    </div>
-                )}
+                { 
+                    authToken && (
+                        <div className='f6 lh-copy gray'>
+                            { link.votes.length } votes | by{' '}
+                            { link.postedBy ? link.postedBy.name : 'Unknown' }{' '}
+                            { timeDifferenceForDate(link.createdAt) }
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
